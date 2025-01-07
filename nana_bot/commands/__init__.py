9c0e9e5c5264f3c461cb51bfe89196ce93c5e2b2
vito <97001360+vito1317@users.noreply.github.com>
2025-01-07 12:00:37 +0800

    update __init__.py

--- nana_bot/commands/__init__.py
+++ nana_bot/commands/__init__.py
@@ -1,5 +1,5 @@
 from .point_command import check_points, add_points, subtract_points
 from .search_command import search
-from .analytics_command import server_analytics
+from .analytics_command import analytics
 from .pass_command import pass_user
 from .aibrowse_command import aibrowse
