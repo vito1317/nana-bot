0b8bda34830a90902db41225c197386df5bda4bd
vito <vito95311@gmail.com>
2025-01-29 21:50:37 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="nana-bot",
-    version="5.6.2.9.4",
+    version="5.6.2.9.5",
     license="MIT",
     author="Vito1317",
     author_email="service@vito95311.online",
