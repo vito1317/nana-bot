1a079eda3bf53b3d4cd227a52c5a8dbab70ceff5
vito <vito95311@gmail.com>
2025-04-29 07:58:46 +0800

    Update and Clean

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="nana-bot",
-    version="5.8.2",
+    version="5.8.3",
     license="MIT",
     author="Vito1317",
     author_email="service@vito95311.online",
@@ -33,7 +33,6 @@ setuptools.setup(
         "gtts",
         "pyttsx3",
         "google-cloud-texttospeech",
-        "ChatTTS",
         "torchaudio",
         "edge_tts"
         ],
